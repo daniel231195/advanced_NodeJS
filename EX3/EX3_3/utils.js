47a5203960ab3e5c223ddcf8e2d0ab8eb6bd88d5
daniel231195 <dansela123@gmail.com>
2023-03-28 12:47:17 +0300

    change

--- EX3/EX3_3/utils.js
+++ EX3/EX3_3/utils.js
@@ -30,47 +30,4 @@ getUserData = (id)=>{
 
 
 
-//
-// const getUserData = (id) =>
-// {
-//     return new Promise((resolve,reject) =>
-//     {
-//         let userdData = {};
-//         userdData.id = id;
-//
-//         jFile.readFile('./users.json', function(err, data)
-//         {
-//             if(err)
-//             {
-//                 reject(err)
-//             }
-//             else
-//             {
-//                 let usersArr = data.users;
-//                 let user = usersArr.find(x => x.id == id);
-//                 userdData.name = user.name;
-//
-//                 jFile.readFile('./phones.json', function(err, data)
-//                 {
-//                     if(err)
-//                     {
-//                         reject(err)
-//                     }
-//                     else
-//                     {
-//                         let phonesArr = data.phones;
-//                         let userPhones = phonesArr.find(x => x.userid == id);
-//
-//                         userdData.phones = userPhones.phones;
-//
-//                         resolve(userdData);
-//
-//                     }
-//                 })
-//             }
-//         })
-//     })
-//
-// }
-
 module.exports = {getUserData}
