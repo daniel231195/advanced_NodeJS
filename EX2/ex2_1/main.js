a9195ddd231f8ecda55ef5e536e3bacd73af8f2c
daniel231195 <dansela123@gmail.com>
2023-03-28 15:07:56 +0300

    EX2

---
+++ EX2/ex2_1/main.js
@@ -0,0 +1,14 @@
+const utils = require('./utils');
+
+/**
+ * reate a module called "StrUtils" which expose a “GetLength”
+ * function that receives A string and returns it's length.
+ * Create another module called “ArrUtils” which expose a “GetArrLength”
+ * function that receives an array of strings and returns
+ * the total lengths of all the strings only by using StrUtils module
+ */
+
+console.log(utils.StrUtils("daniel"))
+
+console.log(utils.ArrUtils(["daniel","sela","bar","ok","yes"]))
+
